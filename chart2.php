aeb11bd718a9575ad0c8a5a31c3695a2de4acc0d
sailor <sailor9066@gmail.com>
2012-12-06 19:31:16 +0800

    12月份上线版本

--- chart2.php
+++ chart2.php
@@ -156,6 +156,7 @@ include_once("getdata.php");
 	</div>
 	</div>
 </div>
+<p><a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=61817708&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:61817708:42" alt="点击这里给我发消息" title="点击这里给我发消息"></a></p>
 </div>
 </div>
 </body>
